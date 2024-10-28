7688e48c96a3480a449b607713307b97e1e21b58
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-28 12:35:07 +0200

    Change setup for js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,29 +1,39 @@
-
 $(function () {
 
     "use strict";
 
-    if (window.innerWidth > 991) {
-        const tl = gsap.timeline({
-            scrollTrigger: {
-                trigger: ".testimonials-ca .sec-head",
-                start: "top",
-                endTrigger: ".testimonials-ca .container",
-                end: "bottom bottom",
-                pin: true,
-                pinSpacing: false
+    var team = new Swiper(".team-marq .team-swiper", {
+        slidesPerView: 4,
+        spaceBetween: 50,
+        centeredSlides: true,
+        speed: 8000,
+        autoplay: {
+            delay: 0,
+        },
+        loop: true,
+        allowTouchMove: false,
+        disableOnInteraction: true,
+        breakpoints: {
+            0: {
+                slidesPerView: 2,
+            },
+            480: {
+                slidesPerView: 2,
+            },
+            787: {
+                slidesPerView: 3,
+            },
+            991: {
+                slidesPerView: 4,
+            },
+            1200: {
+                slidesPerView: 4,
             }
-        });
-    }
+        }
+    });
 
-    gsap.registerPlugin(ScrollTrigger);
-
-    ScrollTrigger.create({
-        trigger: ".work-ca .sec-lg-head",
-        start: "center center",
-        endTrigger: ".work-ca",
-        end: "bottom bottom",
-        pin: true,
+    $('.accordion .accordion-item').on('click', function () {
+        $(this).addClass("active").siblings().removeClass("active");
     });
 
     // Services active
@@ -37,46 +47,36 @@ $(function () {
         }
     });
 
-});
+    $(function () {
+        let cards = gsap.utils.toArray(".cards .card-item");
 
+        let stickDistance = 0;
 
-$(function () {
-    var width = $(window).width();
-    if (width > 991) {
-
-        "use strict";
-
-        $(function () {
-            let cards = gsap.utils.toArray(".cards .card-item");
-
-            let stickDistance = 0;
-
-            let firstCardST = ScrollTrigger.create({
-                trigger: cards[0],
-                start: "center center"
-            });
-
-            let lastCardST = ScrollTrigger.create({
-                trigger: cards[cards.length - 1],
-                start: "bottom bottom"
-            });
-
-            cards.forEach((card, index) => {
-                var scale = 1 - (cards.length - index) * 0;
-                let scaleDown = gsap.to(card, { scale: scale, 'transform-origin': '"50% ' + (lastCardST.start + stickDistance) + '"' });
-
-                ScrollTrigger.create({
-                    trigger: card,
-                    start: "center center",
-                    end: () => lastCardST.start + stickDistance,
-                    pin: true,
-                    pinSpacing: false,
-                    ease: "none",
-                    animation: scaleDown,
-                    toggleActions: "restart none none reverse"
-                });
-            });
+        let firstCardST = ScrollTrigger.create({
+            trigger: cards[0],
+            start: "center center"
         });
 
-    }
+        let lastCardST = ScrollTrigger.create({
+            trigger: cards[cards.length - 1],
+            start: "bottom bottom"
+        });
+
+        cards.forEach((card, index) => {
+            var scale = 1 - (cards.length - index) * 0.025;
+            let scaleDown = gsap.to(card, { scale: scale, 'transform-origin': '"50% ' + (lastCardST.start + stickDistance) + '"' });
+
+            ScrollTrigger.create({
+                trigger: card,
+                start: "center center",
+                end: () => lastCardST.start + stickDistance,
+                pin: true,
+                pinSpacing: false,
+                ease: "none",
+                animation: scaleDown,
+                toggleActions: "restart none none reverse"
+            });
+        });
+    });
+
 });
