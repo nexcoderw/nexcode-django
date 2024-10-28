83c4f08248a80fdffc473bbd00a355c90554b052
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-28 12:24:39 +0200

    Add functions for services page

--- home/views.py
+++ home/views.py
@@ -7,7 +7,19 @@ def about(request):
     return render(request, 'about.html')
 
 def services(request):
-    return render(request, 'services.html')
+    return render(request, 'services/index.html')
+
+def softwareDev(request):
+    return render(request, 'services/softwareDev.html')
+
+def uiUx(request):
+    return render(request, 'services/uiUx.html')
+
+def digitalMarketing(request):
+    return render(request, 'services/digitalMarketing.html')
+
+def mobileDev(request):
+    return render(request, 'services/mobileDev.html')
 
 def portfolio(request):
     return render(request, 'portfolio.html')
