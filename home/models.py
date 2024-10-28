927c53bb6f2c39559a4a6113678e957ee6bbde0e
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-28 13:04:00 +0200

    Add settings model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,50 @@
+import os
+import random
 from django.db import models
+from django.utils.text import slugify
+from imagekit.processors import ResizeToFill
+from imagekit.models import ProcessedImageField
 
-# Create your models here.
+def logo_image_path(instance, filename):
+    base_filename, file_extension = os.path.splitext(filename)
+    random_number = random.randint(1000, 9999)
+    return f'settings/logo/{random_number}_{instance.created_at}{file_extension}'
+
+class Setting(models.Model):
+    black_logo = ProcessedImageField(
+        upload_to=logo_image_path,
+        # processors=[ResizeToFill(600, 600)],
+        format='PNG',
+        options={'quality': 90},
+        null=True,
+        blank=True
+    )
+    white_logo = ProcessedImageField(
+        upload_to=logo_image_path,
+        # processors=[ResizeToFill(600, 600)],
+        format='PNG',
+        options={'quality': 90},
+        null=True,
+        blank=True
+    )
+    address = models.CharField(max_length=255, null=True, blank=True)
+    email = models.CharField(max_length=255, null=True, blank=True)
+    phone_number = models.CharField(max_length=255, null=True, blank=True)
+    instagram = models.CharField(max_length=255, null=True, blank=True)
+    twitter = models.CharField(max_length=255, null=True, blank=True)
+    linkedin = models.CharField(max_length=255, null=True, blank=True)
+    github = models.CharField(max_length=255, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        # Ensure only one instance of settings can exist
+        if not self.pk and Setting.objects.exists():
+            raise ValueError("You can only create one instance of the settings.")
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return "Website Settings"
+
+    class Meta:
+        verbose_name = "Setting"
+        verbose_name_plural = "Settings"
