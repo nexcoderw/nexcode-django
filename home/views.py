6b6b53b5efc72a4a02e95d3018509caecc144407
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-28 12:03:10 +0200

    Add function for each page

--- home/views.py
+++ home/views.py
@@ -2,3 +2,21 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def services(request):
+    return render(request, 'services.html')
+
+def portfolio(request):
+    return render(request, 'portfolio.html')
+
+def team(request):
+    return render(request, 'team.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def contact(request):
+    return render(request, 'contact.html')
