37e18c84118988435e98aa767f02f270f5ec9974
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-28 13:14:48 +0200

    New changes

--- nexcode/urls.py
+++ nexcode/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
