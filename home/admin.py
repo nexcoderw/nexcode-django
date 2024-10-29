c9144348331cf24ae01e813d9f5c48544fbfea32
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-29 09:51:37 +0200

    Register Porfolio model in admin

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,19 @@
 from home.models import *
 from django.contrib import admin
 
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'created_at', 'updated_at', 'display_tags')
+    search_fields = ('name', 'description')
+    list_filter = ('category', 'created_at')
+    ordering = ('-created_at',)
+
+    def display_tags(self, obj):
+        return ", ".join(tag.name for tag in obj.tags.all())
+    display_tags.short_description = 'Tags'
+
+    readonly_fields = ('created_at', 'updated_at')
+
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
     list_display = ('name', 'position', 'created_at', 'updated_at')
