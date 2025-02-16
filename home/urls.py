c93c280d178bd9ce509c7c2493bcad0f1b336a05
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-02-16 11:38:01 +0200

    Add new services routes

--- home/urls.py
+++ home/urls.py
@@ -8,15 +8,22 @@ app_name = 'base'
 urlpatterns = [
     path('', home, name="home"),
     path('about/', about, name="about"),
+
     path('services/', services, name="services"),
     path('services/software-development/', softwareDev, name="softwareDev"),
     path('services/ui-ux/', uiUx, name="uiUx"),
     path('services/digital-marketing/', digitalMarketing, name="digitalMarketing"),
     path('services/mobile-development/', mobileDev, name="mobileDev"),
+    path('services/networking/', networking, name="networking"),
+    path('services/maintenance/', maintenance, name="maintenance"),
+
     path('portfolio/', portfolio, name="portfolio"),
     path('work/<slug>', workDetails, name="workDetails"),
+
     path('team/', team, name="team"),
+
     path('blogs/', blogs, name="blogs"),
     path('blog/<slug>/', getBlogDetails, name="getBlogDetails"),
+
     path('contact/', contact, name="contact"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
