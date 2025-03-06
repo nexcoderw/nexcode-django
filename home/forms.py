4dfab3174015e7810a808104831bc5a38c9d8152
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-06 17:09:56 +0200

    Add TestimonyForm to support addTestimony functionality

--- home/forms.py
+++ home/forms.py
@@ -11,3 +11,20 @@ class ContactForm(forms.ModelForm):
             'subject': forms.TextInput(attrs={'placeholder': 'Subject', 'required': 'true'}),
             'message': forms.Textarea(attrs={'placeholder': 'Your Message', 'rows': 4, 'required': 'true'}),
         }
+
+class TestimonyForm(forms.Form):
+    name = forms.CharField(
+        max_length=255,
+        widget=forms.TextInput(attrs={'placeholder': 'Your Name', 'required': 'true'})
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'placeholder': 'Your Email', 'required': 'true'})
+    )
+    phone_number = forms.CharField(
+        max_length=20,
+        widget=forms.TextInput(attrs={'placeholder': 'Your Phone Number', 'required': 'true'})
+    )
+    image = forms.ImageField(required=False)
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={'placeholder': 'Your Testimony', 'rows': 4, 'required': 'true'})
+    )
