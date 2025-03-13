2025531d968fb090f1de44558be6f5333d5dadf9
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-13 17:04:23 +0200

    New changes

--- nexcode/settings.py
+++ nexcode/settings.py
@@ -74,14 +74,21 @@ WSGI_APPLICATION = 'nexcode.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES =  {
+# DATABASES =  {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.getenv("MYSQL_DB", "nexcodes"),
+#         'USER': os.getenv("MYSQL_USER", "nexcodes_user"),
+#         'PASSWORD': os.getenv("MYSQL_PASSWORD", "R7s!9qB@1vXw"),
+#         'HOST': os.getenv("MYSQL_HOST", "vmi2421856.contaboserver.net"),
+#         'PORT': os.getenv("MYSQL_PORT", "3306"),
+#     }
+# }
+
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("MYSQL_DB", "nexcodes"),
-        'USER': os.getenv("MYSQL_USER", "nexcodes_user"),
-        'PASSWORD': os.getenv("MYSQL_PASSWORD", "R7s!9qB@1vXw"),
-        'HOST': os.getenv("MYSQL_HOST", "vmi2421856.contaboserver.net"),
-        'PORT': os.getenv("MYSQL_PORT", "3306"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
