586447b81beeb4093e91d7e3dc3b8a7bad174b93
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-06-08 16:49:50 +0200

    New changes

--- controller/urls.py
+++ controller/urls.py
@@ -46,5 +46,4 @@ urlpatterns = [
 
     # ---------- Contacts ----------
     path("contacts/", contacts, name="contacts"),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
-  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
