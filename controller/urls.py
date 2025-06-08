ac9834cdf15bf64c7b32d86ee9a00d1b8ac2279e
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-06-08 14:41:08 +0200

    Add login

--- controller/urls.py
+++ controller/urls.py
@@ -6,5 +6,5 @@ from django.conf.urls.static import static
 app_name = 'admin'
 
 urlpatterns = [
-    # 
+    path('', signIn, name="signIn"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- controller/views.py
+++ controller/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from home.forms import *
+from home.models import *
+from django.db.models import Q
+from django.contrib import messages
+from django.shortcuts import render, redirect, get_object_or_404
 
-# Create your views here.
+def signIn(request):
+    settings = Setting.objects.first()
+
+    context = {
+        'settings': settings
+    }
+
+    return render(request, 'admin/auth/login.html', context)
