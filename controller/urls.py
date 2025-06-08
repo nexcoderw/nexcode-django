9cd1d03644f56a64417fabd3ecb9c0b407d4a6d5
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-06-08 16:23:43 +0200

    refactor: switch hard-coded id strings to <int:id> paths in controller.urls

--- controller/urls.py
+++ controller/urls.py
@@ -1,39 +1,45 @@
 from controller.views import *
 from django.conf import settings
-from django.urls import path, re_path
+from django.urls import path
 from django.conf.urls.static import static
 
-app_name = 'admin'
+app_name = "admin"
 
 urlpatterns = [
-    path('', signIn, name="signIn"),
+    path("", signIn, name="signIn"),
+    path("dashboard/", dashboard, name="dashboard"),
 
-    path('dashboard/', dashboard, name="dashboard"),
+    # ---------- Clients ----------
+    path("clients/", clients, name="clients"),
+    path("client/<int:id>/", clientDetails, name="clientDetails"),
+    path("client/<int:id>/update/", updateClient, name="updateClient"),
+    path("client/<int:id>/delete/", deleteClient, name="deleteClient"),
 
-    path('clients/', clients, name="clients"),
-    path('client/id/', clientDetails, name="clientDetails"),
-    path('client/id/update/', updateClient, name="updateClient"),
-    path('client/id/delete/', deleteClient, name="deleteClient"),
+    # ---------- Team ----------
+    path("team/", team, name="team"),
+    path("team/<int:id>/", memberDetals, name="memberDetals"),
+    path("team/<int:id>/update/", updateMember, name="updateMember"),
+    path("team/<int:id>/delete/", deleteMember, name="deleteMember"),
 
-    path('team/', team, name="team"),
-    path('team/id/', memberDetals, name="memberDetals"),
-    path('team/id/update/', updateMember, name="updateMember"),
-    path('team/id/delete/', deleteMember, name="deleteMember"),
+    # ---------- Projects ----------
+    path("projects/", projects, name="projects"),
+    path("project/<int:id>/", projectDetails, name="projectDetails"),
+    path("project/<int:id>/update/", updateProject, name="updateProject"),
+    path("project/<int:id>/delete/", deleteProject, name="deleteProject"),
 
-    path('projects/', projects, name="projects"),
-    path('project/id/', projectDetails, name="projectDetails"),
-    path('project/id/update/', updateProject, name="updateProject"),
-    path('project/id/delete/', deleteProject, name="deleteProject"),
+    # ---------- Blogs ----------
+    path("blogs/", blogs, name="blogs"),
+    path("blog/<int:id>/", blogDetails, name="blogDetails"),
+    path("blog/<int:id>/update/", updateBlog, name="updateBlog"),
+    path("blog/<int:id>/delete/", deleteBlog, name="deleteBlog"),
 
-    path('blogs/', blogs, name="blogs"),
-    path('blog/id/', blogDetails, name="blogDetails"),
-    path('blog/id/update/', updateBlog, name="updateBlog"),
-    path('blog/id/delete/', deleteBlog, name="deleteBlog"),
+    # ---------- Testimonies ----------
+    path("testimonies/", testimonies, name="testimonies"),
+    path("testimont/<int:id>/", testimonyDetails, name="testimonyDetails"),
+    path("testimont/<int:id>/update/", updateTestimony, name="updateTestimony"),
+    path("testimont/<int:id>/delete/", deleteTestimony, name="deleteTestimony"),
 
-    path('testimonies/', testimonies, name="testimonies"),
-    path('testimont/id/', testimonyDetails, name="testimonyDetails"),
-    path('testimont/id/update/', updateTestimony, name="updateTestimony"),
-    path('testimont/id/delete/', deleteTestimony, name="deleteTestimony"),
-
-    path('contacts/', contacts, name="contacts"),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # ---------- Contacts ----------
+    path("contacts/", contacts, name="contacts"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
