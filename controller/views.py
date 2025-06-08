b532982d7eea6d8f2c3b6c5d67b657f5decf0e9a
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-06-08 17:57:08 +0200

    feat: add search & pagination to clients view

--- controller/views.py
+++ controller/views.py
@@ -3,6 +3,7 @@ from home.models import *
 from django.db.models import Q
 from django.contrib import messages
 from django.shortcuts import render, redirect, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def signIn(request):
     settings = Setting.objects.first()
