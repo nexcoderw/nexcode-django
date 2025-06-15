0f8334e906833e571dd6058fffcfa8e4d2a35b7b
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-06-15 21:06:35 +0200

    Add URL route for trainingDetail view with slug parameter

--- home/urls.py
+++ home/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('testimony/', addTestimony, name="addTestimony"),
 
     path('training/', getTraining, name="getTraining"),
+    path('training/<slug:slug>/', trainingDetail, name='training_detail'),
 
     path('contact/', contact, name="contact"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
