5fd5b221d16ce404ae6613e1ae05c29a3211085a
unknown <kajugadaniels@gmail.com>
2025-08-05 16:24:17 +0200

    New chnges

--- nexcode/settings.py
+++ nexcode/settings.py
@@ -79,23 +79,23 @@ WSGI_APPLICATION = 'nexcode.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES =  {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("MYSQL_DB"),
-        'USER': os.getenv("MYSQL_USER"),
-        'PASSWORD': os.getenv("MYSQL_PASSWORD"),
-        'HOST': os.getenv("MYSQL_HOST"),
-        'PORT': os.getenv("MYSQL_PORT", 3306),
-    }
-}
-
-# DATABASES = {
+# DATABASES =  {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.getenv("MYSQL_DB"),
+#         'USER': os.getenv("MYSQL_USER"),
+#         'PASSWORD': os.getenv("MYSQL_PASSWORD"),
+#         'HOST': os.getenv("MYSQL_HOST"),
+#         'PORT': os.getenv("MYSQL_PORT", 3306),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
