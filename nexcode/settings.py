7b1af32ac582a632abb70a4c5ce76b208de896b2
kajugadaniels <kajugadaniels@gmail.com>
2025-11-19 11:50:34 +0200

    New changes

--- nexcode/settings.py
+++ nexcode/settings.py
@@ -90,14 +90,21 @@ WSGI_APPLICATION = 'nexcode.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv("POSTGRES_DB", ""),
+#         'USER': os.getenv("POSTGRES_USER", ""),
+#         'PASSWORD': os.getenv("POSTGRES_PASSWORD", ""),
+#         'HOST': os.getenv("POSTGRES_HOST", ""),
+#         'PORT': os.getenv("POSTGRES_PORT", ""),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("POSTGRES_DB", ""),
-        'USER': os.getenv("POSTGRES_USER", ""),
-        'PASSWORD': os.getenv("POSTGRES_PASSWORD", ""),
-        'HOST': os.getenv("POSTGRES_HOST", ""),
-        'PORT': os.getenv("POSTGRES_PORT", ""),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
