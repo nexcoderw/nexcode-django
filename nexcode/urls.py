221c814ad250b25ce0b4de6fe60f830d7a8c3daa
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-09-21 22:29:05 +0200

    feat(seo): expose sitemap and robots endpoints

--- nexcode/urls.py
+++ nexcode/urls.py
@@ -1,14 +1,18 @@
-from django.contrib import admin
 from django.conf import settings
-from django.urls import path, include
 from django.conf.urls.static import static
-from home.views import healthcheck
+from django.contrib import admin
+from django.urls import include, path
+
+from home.sitemaps import public_sitemap
+from home.views import healthcheck, robots
 
 urlpatterns = [
-    path('health/', healthcheck, name='healthcheck'),
-    path('', include('home.urls')),
-    path('admin/', admin.site.urls),
-    path('ckeditor/', include('ckeditor_uploader.urls')),
+    path("robots.txt", robots, name="robots"),
+    path("sitemap.xml", public_sitemap, name="sitemap"),
+    path("health/", healthcheck, name="healthcheck"),
+    path("", include("home.urls")),
+    path("admin/", admin.site.urls),
+    path("ckeditor/", include("ckeditor_uploader.urls")),
 ]
 
 if settings.DEBUG:
