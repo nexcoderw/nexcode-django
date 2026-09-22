72a23806a68871a0b696ec2b20ab8a3cc14bce96
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-09-22 16:57:25 +0200

    feat(team): assign an independent URL to every Team operation

--- admin_api/urls/team.py
+++ admin_api/urls/team.py
@@ -1,6 +1,20 @@
 from django.urls import path
 
-from admin_api.views import team
+from admin_api.views.team.add import (
+    add_team_view,
+)
+from admin_api.views.team.delete import (
+    delete_team_view,
+)
+from admin_api.views.team.detail import (
+    team_detail_view,
+)
+from admin_api.views.team.list import (
+    list_team_view,
+)
+from admin_api.views.team.update import (
+    update_team_view,
+)
 
 
 app_name = "team"
@@ -8,13 +22,28 @@ app_name = "team"
 
 urlpatterns = [
     path(
-        "",
-        team.team_collection_view,
-        name="collection",
+        "list/",
+        list_team_view,
+        name="list",
     ),
     path(
-        "<int:team_id>/",
-        team.team_item_view,
-        name="item",
+        "add/",
+        add_team_view,
+        name="add",
+    ),
+    path(
+        "detail/<int:team_id>/",
+        team_detail_view,
+        name="detail",
+    ),
+    path(
+        "update/<int:team_id>/",
+        update_team_view,
+        name="update",
+    ),
+    path(
+        "delete/<int:team_id>/",
+        delete_team_view,
+        name="delete",
     ),
 ]
