f998640d224045003170f462d9014b9034a99bb6
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-09-24 21:57:46 +0200

    feat(contact): route admin contact endpoints

--- admin_api/urls/contact.py
+++ admin_api/urls/contact.py
@@ -3,6 +3,15 @@ from django.urls import path
 from admin_api.views.contact.add import (
     add_contact_view,
 )
+from admin_api.views.contact.detail import (
+    contact_detail_view,
+)
+from admin_api.views.contact.list import (
+    list_contact_view,
+)
+from admin_api.views.contact.reply import (
+    reply_contact_view,
+)
 
 
 app_name = "contact"
@@ -13,5 +22,20 @@ urlpatterns = [
         "add/",
         add_contact_view,
         name="add",
+    ),
+    path(
+        "list/",
+        list_contact_view,
+        name="list",
+    ),
+    path(
+        "detail/<int:contact_id>/",
+        contact_detail_view,
+        name="detail",
+    ),
+    path(
+        "reply/<int:contact_id>/",
+        reply_contact_view,
+        name="reply",
     ),
 ]
